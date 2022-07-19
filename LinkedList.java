db512bf0a884fec45853fd722e04e5c2e6508557
Akumar132232 <64557746+Akumar132232@users.noreply.github.com>
2022-07-19 18:24:44 +0530

    Created Link List by Appending numbers.

---
+++ LinkedList.java
@@ -0,0 +1,38 @@
+package com.bridglabz;
+
+public class LinkedList<T> {
+    Node<T> head;
+    Node<T> tail;
+
+    public void push(T data) {
+        Node<T> obj1 = new Node<>(data);
+        if (obj1 == null) {
+            head = obj1;
+            tail = obj1;
+        } else {
+            obj1.next = head;
+            head = obj1;
+        }
+    }
+    public void add(T data) {
+        Node<T> obj2= new Node<>(data);
+        if (head == null) {
+            head = obj2;
+            tail = obj2;
+        } else {
+            tail.next=obj2;
+            tail = obj2;
+        }
+    }
+
+
+    public void print() {
+        Node<T> temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+
+
+        }
+    }
+}
