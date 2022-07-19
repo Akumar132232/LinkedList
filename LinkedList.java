136458d845e18f2a16d08be3d5deabd4073d2ba9
Akumar132232 <64557746+Akumar132232@users.noreply.github.com>
2022-07-19 18:28:53 +0530

    Deleting the First Element from Linked List

---
+++ LinkedList.java
@@ -0,0 +1,50 @@
+package com.bridglabz;
+
+public class LinkedList<T> {
+    Node<T> head;
+    Node<T> tail;
+
+    public void push(T data) {
+        Node<T> obj1 = new Node<>(data);
+        if (obj1 == null) {
+            head = obj1;
+            tail = obj1;
+        } else {
+            obj1.next = head;
+            head = obj1;
+        }
+    }
+    public void add(T data) {
+        Node<T> obj2= new Node<>(data);
+        if (head == null) {
+            head = obj2;
+            tail = obj2;
+        } else {
+            tail.next=obj2;
+            tail = obj2;
+        }
+    }
+    public void insertBetween(T insertData) {
+        Node<T> obj2= new Node<>(insertData);
+        head.next=obj2;
+        obj2.next=tail;
+    }
+
+    public void print() {
+        Node<T> temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+    }
+            public T pop() {
+                Node<T> popElement = head;
+                T element = popElement.data;
+                Node<T> nextEle = popElement.next;
+                popElement.data = null;
+                popElement.next = null;
+                head = nextEle;
+                return element;
+
+        }
+    }
