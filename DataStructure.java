2e1d9baacf35dc2d6e8ca4850729730ed3c33057
Akumar132232 <64557746+Akumar132232@users.noreply.github.com>
2022-07-19 18:28:24 +0530

    Deleting the First Element from Linked List

---
+++ DataStructure.java
@@ -0,0 +1,17 @@
+package com.bridglabz;
+
+public class DataStructure {
+    public static void main(String[] args) {
+        LinkedList<Integer> LinkedList=new LinkedList();
+        LinkedList.push(70);
+        LinkedList.push(30);
+        LinkedList.push(56);
+
+        LinkedList.print();
+
+        Integer data = LinkedList.pop();
+        System.out.println(data);
+
+        LinkedList.print();
+    }
+}
